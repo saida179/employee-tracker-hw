709d420d4b4c592324b1ceb1adafa29ec3fb9766
saida179 <69127945+saida179@users.noreply.github.com>
2020-11-04 21:39:49 -0700

    working on the emloyee js

--- employee_tracker_db.js
+++ employee_tracker_db.js
@@ -1,9 +1,116 @@
 const mysql = require("mysql2");
+const inquirer = require("inquirer")
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "2017hi!",
     database: "employee_tracker_db"
   });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    employment();
+  });
+
+  function employment() {
+    inquirer.prompt([
+        {
+            type: "",
+            name: "",
+            message: "",
+            choices: [""]
+        }
+
+        //Build a command-line application that at a minimum allows the user to:
+
+
+        function addEmployees() {
+          inquirer
+              .prompt([
+                  {
+                      name: "",
+                      type: "",
+                      message: ""
+                  }
+              ])
+      };
+      
+      function addRole() {
+          inquirer
+              .prompt([
+                  {
+                      name: "",
+                      type: "",
+                      message: "",
+                  }
+              ])
+      };
+      
+      function addDepartment() {
+          inquirer
+              .prompt([
+                  {
+                      name: "",
+                      type: "",
+                      message: "",
+                  }
+              ])
+      };
+      
+      
+      function viewEmployees() {};
+      
+      function viewRole() {};
+      
+      function viewDepartments() {};
+      
+
+
+
+      // Bonus points using the ice_creamCRUD activity for help
+      
+      
+
+ // function updateProduct() {
+ //   console.log("Updating all Rocky Road quantities...\n");
+//    var query = connection.query(
+ //     "UPDATE products SET ? WHERE ?",
+ //     [
+ //       {
+ //         quantity: 100
+ //       },
+ //       {
+ //         flavor: "Rocky Road"
+ //       }
+ //     ],
+ //     function(err, res) {
+ //       if (err) throw err;
+ //       console.log(res.affectedRows + " products updated!\n");
+ //       // Call deleteProduct AFTER the UPDATE completes
+  //      deleteProduct();
+ //     }
+ //   );
+  
+    // logs the actual query being run
+  //  console.log(query.sql);
+ // }
+
+
+ // function deleteProduct() {
+   // console.log("Deleting all strawberry icecream...\n");
+    //connection.query(
+     // "DELETE FROM products WHERE ?",
+     // {
+     //   flavor: "strawberry"
+     // },
+     // function(err, res) {
+      //  if (err) throw err;
+      //  console.log(res.affectedRows + " products deleted!\n");
+        // Call readProducts AFTER the DELETE completes
+      //  readProducts();
+     // }
+    //);
+ // }
--- employee_tracker_db.sql
+++ employee_tracker_db.sql
@@ -21,3 +21,17 @@ CREATE TABLE employee (
     role_id INT NOT NULL,
     manager_id INT NOT NULL
 );
+
+INSERT INTO department (deptarment_name)
+VALUES ("");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Human")
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Manager", 100000.00, 1);
+
+
+
+
+
