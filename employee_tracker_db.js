9191e081f9deb9aca9eb5ffd7c043b585006d2cf
saida179 <69127945+saida179@users.noreply.github.com>
2020-11-10 18:36:36 -0700

    working on my js

--- employee_tracker_db.js
+++ employee_tracker_db.js
@@ -7,76 +7,142 @@ var connection = mysql.createConnection({
     user: "root",
     password: "2017hi!",
     database: "employee_tracker_db"
-  });
+});
 
-  connection.connect(function(err) {
+connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     employment();
-  });
+});
 
-  function employment() {
-    inquirer.prompt([
+    function employment() {
+        inquirer.prompt([
         {
             type: "list",
             name: "work",
             message: "choose one of the following",
-            choices: ["Add Employee", "Add Employee Role", "Add Department", "View Employees", "View Employee Role", "View Departments", "Update Employee", "Delete Employee", "Delete Employee Role", "Delete Department"]
+            choices: ["Add Employee", "Add Role", "Add Department", "View Employees", "View Employee Role", "View Departments", "Update Employee", "Delete Employee", "Delete Employee Role", "Delete Department"]
         }
-
-      ]).then(answers => {
-        if (answers.work === "Exit") {
-            connection.end();
 
         //Build a command-line application that at a minimum allows the user to:
 
 
         function addEmployees() {
-          inquirer
-              .prompt([
-                  {
-                      name: "",
-                      type: "",
-                      message: ""
-                  }
-              ])
-      };
+            connection.query("SELECT * FROM role", function (err, data) {
+                if (err) throw err;
+                console.log(data)
+                let role = data.map(role => {
+                    return { name: role.title, value: role.id }
+                })
+        inquirer
+            .prompt([
+                {
+                    name: "input",
+                    type: "first_name",
+                    message: "Name of employee your add."
+                },
+                {
+                type: "input",
+                name: "last_name",
+                message: "Last name of the employee your add:"
+            },
+            {
+                type: "list",
+                name: "role_id",
+                message: "Add employee role:",
+                choices: role
+            },
+            {
+                type: "list",
+                name: "manager_id",
+                message: "Add manager for employee:",
+                choices: manager
+            }
+            ]).then(answers => {
+                connection.query("INSERT INTO department (name) VALUES (?)", [answers.name], function (err, res) {
+                    if (err) throw err;
+                    console.table(res);
+                    employment();
+                })
+            })
+    };
+    
+    function addRole() {
+        connection.query("SELECT * FROM department", function (err, data) {
+            if (err) throw err;
+            console.log(data)
+            let department = data.map(dept => {
+                return { name: name, value: dept.id }
+            })
+        inquirer
+            .prompt([
+                {
+                    name: "input",
+                    type: "title",
+                    message: "What Title would you like to add",
+                },
+                {
+                    name: "input",
+                    type: "department_id",
+                    message: "what role will department take?",
+                },
+                {
+                    name: "input",
+                    type: "salary",
+                    message: "Salary or each title",
+                }
+            ]).then(answers => {
+                connection.query("INSERT INTO role (title, salary, department_id) VALUES ", function (err, data) {
+                    if (err) throw err;
+                    console.log(data);
+                    employment();
+                })
+            })
+    }
       
-      function addRole() {
-          inquirer
-              .prompt([
-                  {
-                      name: "",
-                      type: "",
-                      message: "",
-                  }
-              ])
-      };
+    function addDepartment() {
+        inquirer
+            .prompt(
+                {
+                    name: "input",
+                    type: "department_name",
+                    message: "Name of the department you would like to add",
+                }
+            ).then(answers => {
+                connection.query("INSERT INTO department (department_name) VALUES ()", [answers.department_name], function (err, res) {
+                    if (err) throw err;
+                    console.table(res);
+                    employment();
+    }
+    
+    function viewDepartments() {
+        connection.query("SELECT * FROM department", function (err, data) {
+            if (err) throw err;
+            console.table(data);
+            employment()
+        })
+    }
       
-      function addDepartment() {
-          inquirer
-              .prompt([
-                  {
-                      name: "",
-                      type: "",
-                      message: "",
-                  }
-              ])
-      };
-      
-      
-      function viewEmployees() {};
-      
-      function viewRole() {};
-      
-      function viewDepartments() {};
-      
+    function viewEmployees() {
+        connection.query("SELECT * FROM employee", function (err, data) {
+            if (err) throw err;
+            console.table(data)
+            employment();
+        })
+    }
+    
+    function viewRoles() {
+        connection.query("SELECT * FROM role", function (err, data) {
+            if (err) throw err;
+            console.table(data)
+            employment();
+        })
+    
+    }
 
 
 
       // Bonus points using the ice_creamCRUD activity for help
-      
-      
 
  // function updateProduct() {
  //   console.log("Updating all Rocky Road quantities...\n");
